ab6ac3ecd15069a59785d45a16fe7be91c2c4e52
aerotog <aerotog@gmail.com>
2015-12-11 18:36:42 -0600

    Add solution

---
+++ LinkedList/LinkedList/LinkedListNode.cs
@@ -0,0 +1,14 @@
+﻿namespace LinkedList
+{
+    public class LinkedListNode
+    {
+        public static int Data { get; private set; }
+        public static LinkedListNode Next { get; private set; }
+
+        public LinkedListNode(int data, LinkedListNode next)
+        {
+            Data = data;
+            Next = next;
+        }
+    }
+}
